fe723bb2b48e6dbd205b1712d5b9e984339a5266
gaeguli <105928069+gaeguli@users.noreply.github.com>
2025-08-13 20:21:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ def main():
         data='/home/user/ai_hackathon/dataset/data.yaml',
         epochs=300,               
         batch=16,                 
-        imgsz=768,                 # 이미지 크기 확대 (메모리 허용 시)
+        imgsz=768,
         workers=4,                 
         device=0,                  
         patience=300,               
         optimizer="AdamW",           
-        lr0=0.000000000000000001,                 # 기본 학습률 증가
-        # lr_scheduler='cosine',     # cosine learning rate scheduler 추가
+        lr0=0.0001,                 
         weight_decay=0.0005,       
-        augment=True,              # 내장 증강 유지
-        # ema=True
+        augment=True
     )
 
     # results = model.val()
